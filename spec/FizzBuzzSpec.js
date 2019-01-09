476d9fca226b10c1423b5bb1dff59b9a39c9284a
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 20:49:55 +0000

    RED: returns buzz if num divisible by 5

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -3,12 +3,16 @@ describe('FizzBuzz', function() {
   var fizzbuzz;
 
   beforeEach(function() {
-    fizzbuzz = new FizzBuzz()
-  })
+    fizzbuzz = new FizzBuzz();
+  });
 
   describe('when given a number', function() {
     it('returns Fizz if it is divisible by 3', function() {
       expect(fizzbuzz.play(3)).toBe('Fizz')
+    })
+
+    it('returns Buzz if it is divisivly by 5', function() {
+      expect(fizbuzz.play(5)).toBe('Buzz')
     })
   })
 
