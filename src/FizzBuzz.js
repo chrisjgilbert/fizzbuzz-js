343a9af80dc90e124dbcf6afbeab10e6d670c5dd
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-09 21:04:03 +0000

    REFACTOR: refactored div by 15 into private helper method

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -2,7 +2,7 @@ var FizzBuzz = function() {}
 
 FizzBuzz.prototype = {
   play: function(number) {
-    if (number % 15 === 0) {
+    if (this._divisibleBy15(number)) {
       return 'FizzBuzz'
     } else if (this._divisibleBy5(number)) {
       return 'Buzz'
@@ -13,5 +13,9 @@ FizzBuzz.prototype = {
 
   _divisibleBy5: function(number) {
     if (number % 5 ===0 ) return true
+  },
+
+  _divisibleBy15: function(number) {
+    if (number % 15 ===0 ) return true
   }
 }
